0356f9f8bdc4d78cc7c0e861637ef893a4226309
Athos Couto <athos@chiselstrike.com>
2022-09-14 10:53:48 -0300

    npm run init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// This is the main file for the Netlify Build plugin {{name}}.
+// This is the main file for the Netlify Build plugin chiselstrike.
 // Please read the comments to learn more about the Netlify Build plugin syntax.
 // Find more information in the Netlify documentation.
 
@@ -16,7 +16,7 @@ export const onPreBuild = async function ({
   // For example:
   //
   //   [[plugins]]
-  //   package = "netlify-plugin-{{name}}"
+  //   package = "netlify-plugin-chiselstrike"
   //     [plugins.inputs]
   //     foo = "bar"
   inputs,
